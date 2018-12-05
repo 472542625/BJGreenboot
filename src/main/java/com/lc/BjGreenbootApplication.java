050348aef2ce8fc60b37710e3e22c62e56d698b4
litao <472542625@qq.com>
2018-12-05 16:24:29 +0800

    Initial commit

---
+++ src/main/java/com/lc/BjGreenbootApplication.java
@@ -0,0 +1,12 @@
+package com.lc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjGreenbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BjGreenbootApplication.class, args);
+	}
+}
